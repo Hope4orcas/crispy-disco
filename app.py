2cea339498a7cbc861af570969db98eff3a30f01
Hope4orcas <124528587+Hope4orcas@users.noreply.github.com>
2023-04-25 17:31:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.title("Image Editor")
 filters = ["Contour","Detail","Blur","Sharpen"]
 
 with st.sidebar:
-  filters_selected = st.multiselect("Elige")
+  filters_selected = st.multiselect("Elige",filters)
   
 fichero = st.file_uploader("Elige tu imagen")
