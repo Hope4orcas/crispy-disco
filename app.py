b402451965303abb626d2b15a07e527fb32f34e3
Hope4orcas <124528587+Hope4orcas@users.noreply.github.com>
2023-04-25 17:28:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import streamlit as st
 
 st.title("Image Editor")
 
+filters = ["Contour","Detail","Blur","Sharpen"]
+
 with st.sidebar():
-  filters = st.multiselect("Elige",["Contour","Detail","Blur","Sharpen"])
+  filters_selected = st.multiselect("Elige")
   
 fichero = st.file_uploader("Elige tu imagen")
