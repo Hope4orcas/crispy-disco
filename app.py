5131a89c12af1ca5fc1a281c443b9b2c9065508e
Hope4orcas <124528587+Hope4orcas@users.noreply.github.com>
2023-04-25 17:38:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,11 @@ with st.sidebar:
   filters_selected = st.multiselect("Elige",filters)
   
 fichero = st.file_uploader("Elige tu imagen", type=['png','jpg'])
+
+col1,col2 = st.columns(2)
+
+with col1:
+  st.header("Original")
+  
+with col2:
+  st.header("Edited")
