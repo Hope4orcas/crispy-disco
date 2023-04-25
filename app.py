3984d65965fd32c66f43d0923edd24d27896cd88
Hope4orcas <124528587+Hope4orcas@users.noreply.github.com>
2023-04-25 17:34:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ filters = ["Contour","Detail","Blur","Sharpen"]
 with st.sidebar:
   filters_selected = st.multiselect("Elige",filters)
   
-fichero = st.file_uploader("Elige tu imagen")
+fichero = st.file_uploader("Elige tu imagen", type=['png','jpg'])
